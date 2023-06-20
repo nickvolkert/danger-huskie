c71131f0352dc4fe066a75d290c885589f1b5220
Nick Volkert <nick@nickvolkert.com>
2023-06-20 10:26:56 -0500

    sitemap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ import HelmetMeta from "../components/HelmetMeta";
 
 let source = DangerHuskie;
 let x = 0;
-let y = 9; //Chicago Dog with Flag
+let y = 29; //Chicago Dog with Flag
 
 let pagename = CoreSite[x].title;
 let subhead = CoreSite[x].heading;
